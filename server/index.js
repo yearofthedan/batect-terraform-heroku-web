fc5cd19270b000daaf181bcbc87f8053fb6db7cb
Dan Mutton <YearOfTheDan@users.noreply.github.com>
2019-09-08 19:21:57 +1000

    Add a backend server for the nginx app to proxy to

---
+++ server/index.js
@@ -0,0 +1,6 @@
+import express from 'express';
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => res.send('Hello I am a server'));
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
